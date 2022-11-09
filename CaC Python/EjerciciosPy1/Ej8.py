f767812dbc967edc4cf1a5d2c2097f47be53a1a0
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-09 20:54:24 -0300

    7 y 8

---
+++ CaC Python/EjerciciosPy1/Ej8.py
@@ -0,0 +1,7 @@
+# m = input("Introduce un número: ")
+# n = input("Introduce otro número: ")
+# print(m + " dividido " +  n + " da " + str(int(m) // int(n)) + " con resto " + str(int(m) % int(n)))
+
+m = int(input("Introduce un número: "))
+n = int(input("Introduce otro número: "))
+print(m, "dividido ", n, "da", m//n, "con resto ", m%n)
