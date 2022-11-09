e228773c02fe728645eb8dc6271874369ad22166
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-09 20:49:43 -0300

    5 y6

---
+++ CaC Python/EjercicioPy1/Ej5.py
@@ -0,0 +1,7 @@
+nombre = input("Ingrese su nombre: ")
+numero_entero = int(input("Ingrese un numero: "))
+
+print()
+
+for i in range(numero_entero):
+  print(str(i+1) + " " + nombre)
---
+++ CaC Python/EjercicioPy1/Ej6.py
@@ -0,0 +1,3 @@
+nombre = input("¿Cómo te llamas? ")
+#print(nombre + " tiene " + str(len(nombre)) + " letras")
+print(nombre, "tiene ",len(nombre), "letras")
