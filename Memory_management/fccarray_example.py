da872118dfbad1d4f78b01155c10a5da421228ae
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-11 19:41:32 -0300

    ahira si los subo

---
+++ Memory_management/fccarray_example.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+
+A = np.array([
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i']
+])
+
+print(A[:, :2])
---
+++ Memory_management/slicestringbycharacters.py
@@ -0,0 +1,18 @@
+#Initialize the String
+String = ['P', 'r', 'e', 'p', 'I', 'n', 's', 't', 'a']
+
+#Slicing using 1 negative index
+arr = String[-2:]
+print(arr)
+
+#Slicing using 1 negative index
+arr = String[:-3]
+print(arr)
+
+#Slicing using 3rd negative index
+arr = String[::-1]
+print(arr)
+
+#Slicing using 2 negative indexes
+arr = String[-9:-4]
+print(arr)
