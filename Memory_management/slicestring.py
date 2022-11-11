fe1fa228f07f2ebfc7fc9e45ea9ce70165b379fc
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-11 19:40:27 -0300

    subo ejemplos de comportamientos en memoria con strings y tuplas

---
+++ Memory_management/slicestring.py
@@ -0,0 +1,20 @@
+#Initialize the String
+String = 'PrepInsta'
+
+#Slicing using 1 negative index
+arr = String[-2:]
+print(arr)
+
+#Slicing using 1 negative index
+arr = String[:-3]
+print(arr)
+
+#Slicing using 3rd negative index
+arr = String[::-1]
+print(arr)
+
+#Slicing using 2 negative indexes
+arr = String[-9:-4]
+print(arr)
+
+
