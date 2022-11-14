f55c4d62453ee0f0a7cb5b86093c50abb78a2f0a
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-13 22:47:45 -0300

    9

--- CaC Python/EjerciciosPy1/Ej9.py
+++ CaC Python/EjerciciosPy1/Ej9.py
@@ -2,9 +2,8 @@ inversion = int(input("Introduce la cantidad a invertir: "))
 interes= int(input("Introduce el porcentaje de interés anual: "))
 anos= int(input("Introduce el número de años de inversión: "))
 capital = 0
-
 for i in range(anos):
   inversion = inversion + (inversion * (interes *0.01)) 
   capital += inversion
 
-print("el interes obtenido por la inversion en", anos, " años es de: ", capital*(interes*0.01)*anos "%")
+print("el interes obtenido por la inversion en", anos, " años es de: ", capital*(interes*0.01)*anos, "%")
