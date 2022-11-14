826ce6ca76344186385b0c5bfe7a80ef9d2b2819
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-14 00:16:23 -0300

    9

--- CaC Python/EjerciciosPy1/Ej9.py
+++ CaC Python/EjerciciosPy1/Ej9.py
@@ -1,14 +1,25 @@
+#inversion = float(input("Introduce la cantidad a invertir: "))
+#interes= int(input("Introduce el porcentaje de interés anual: "))
+#anos= int(input("Introduce el número de años de inversión: "))
+#
+#'''
+#capital = 0
+#
+#for i in range(anos):
+#  inversion = inversion + (inversion * (interes *0.01)) 
+#  capital += inversion
+#  '''
+#
+#print("el interes obtenido por la inversion en", anos, " años es de: ", inversion*(interes*0.01)*anos)
 
+# amount = float(input("¿Cantidad a invertir? "))
+# interest = float(input("¿Interés porcentual anual? "))
+# years = int(input("¿Años? "))
+# for i in range(years):
+#     amount = (amount * (1 + interest))
+#     print("Capital tras " + str(i+1) + " años: " + str(round(amount)))
 
-
-inversion = int(input("Introduce la cantidad a invertir: "))
-interes= int(input("Introduce el porcentaje de interés anual: "))
-anos= int(input("Introduce el número de años de inversión: "))
-
-capital = 0
-
-for i in range(anos):
-  inversion = inversion + (inversion * (interes *0.01)) 
-  capital += inversion
-
-print("el interes obtenido por la inversion en", anos, " años es de: ", capital*(interes*0.01)*anos, "%")
+amount = float(input("¿Cantidad a invertir? "))
+interest = float(input("¿Interés porcentual anual? "))
+years = int(input("¿Años? "))
+print(round(amount * (1 + interest)**years))
