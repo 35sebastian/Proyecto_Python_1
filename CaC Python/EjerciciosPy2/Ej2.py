9d4e98263837c3ce5496966d0efc9e5bf96ba52c
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-15 01:52:55 -0300

    2

--- CaC Python/EjerciciosPy2/Ej2.py
+++ CaC Python/EjerciciosPy2/Ej2.py
@@ -1,6 +1,38 @@
-name= "sebastian"
-password= "Holacodo"
+from getpass import getpass as gp   # Con la librería getpass conseguimos que la contraseña pueda permanecer oculta mientras es ingresada.
 
-inname= int(input("Ingrese su nombre:"))
-inpass= int(input("Ingrese su contraseña:"))
+nombre = input("Ingrese el nombre de USUARIO: \n")
+print("Ingrese la CONTRASEÑA: \t")
+contrasena = gp()
 
+print("*****************************************")
+print("         E J E R C I C I O N° 2")
+print("*****************************************")
+print()
+nombreNuevo = input("Ingrese el nombre de USUARIO: \n")
+print("Ingrese la CONTRASEÑA: \t")
+contrasenaNueva = gp()
+
+if nombre == nombreNuevo and contrasena == contrasenaNueva:
+  print("Bienvenido %s, te has logueado exitosamente." %nombreNuevo)
+else:
+  print("Los datos ingresados son incorrectos.")
+
+#       
+#       nombre = input("Ingrese el nombre de USUARIO: \n")
+#       contrasena = input("Ingrese la CONTRASEÑA: \t")
+#       
+#       
+#       print("*****************************************")
+#       print("         E J E R C I C I O N° 2")
+#       print("*****************************************")
+#       print()
+#       nombreNuevo = input("Ingrese el nombre de USUARIO: \n")
+#       contrasenaNueva = input("Ingrese la CONTRASEÑA: \t")
+#       
+#       
+#       if nombre == nombreNuevo and contrasena == contrasenaNueva:
+#         print("Bienvenido %s, te has logueado exitosamente." %nombreNuevo)
+#       else:
+#         print("Los datos ingresados son incorrectos.")
+#       
+#       
