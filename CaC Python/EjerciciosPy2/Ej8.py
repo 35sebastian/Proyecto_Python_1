9f57569dc4a95a915237b63bf5264245791106e6
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2022-11-17 02:44:28 -0300

    8

---
+++ CaC Python/EjerciciosPy2/Ej8.py
@@ -0,0 +1,9 @@
+print("*****************************************")
+print("         E J E R C I C I O N° 8")
+print("*****************************************")
+print()
+for i in range (1, 11):
+  print("TABLA DEL %i:" %(i))
+  for j in range (11):
+    print("%d x %d = %d" %(i, j, i*j))
+  print()
