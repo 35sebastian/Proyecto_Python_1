07cc7ec5d08eb5064b8ab084cec4121575d0ddab
35sebastian <vegaa.sebastian@gmail.com>
2023-01-29 10:02:53 -0300

    funciones.html y sumar.js

---
+++ ArgentinaPrograma/JS/sumar.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById('sumar').addEventListener('click', function(){
+        
+    let numeroA = documnet.getElementById('numero1').value;
+    console.log('El valor del numero A es: ' +numeroA)
+  
+    let numeroB = documnet.getElementById('numero2').value;
+    console.log('El valor del numero B es: ' +numeroB)
+
+    let resultado = sumar(parseInt(numeroA) , parseInt(numeroB));
+    console.log('El resultado de la suma es: ' +resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+    document.getElementById('ContenedorResultado').style.display = 'block';
+    
+});
+
+function sumar (a,b){
+    return a + b;
+}
